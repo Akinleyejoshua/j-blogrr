41811a77e88eb13e7c81844f8dfeb57b544f9045
Joshua <akinleyejoshua.dev@gmail.com>
2020-11-30 20:41:30 +0100

    Add files via upload

--- src/reducers/publisher.js
+++ src/reducers/publisher.js
@@ -1,9 +1,20 @@
 const initialState = {
-    
+    title: "",
+    content: "",
 }
 
 const publisherReducer = (state = initialState, action) => {
     switch(action.type){
+        case "PUBLISH_TITLE":
+            return{
+                ...state,
+                title: action.payload
+            }
+        case "PUBLISH_CONTENT":
+            return {
+                ...state,
+                content: action.payload
+            }
         default:
             return state
     }
